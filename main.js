5a3e406d8d5cc3696d9c9146c63b91bb77f76780
egpennington <egpennington@hotmail.com>
2024-09-14 10:25:29 -0800

    add js function for arr

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+import { menuArray } from "/data.js"
 const message = 'Hello World'
 
-console.log(message)
+
+function getFoodItemArr(foodArr) {
+    return menuArray.map(foodItem => {
+        const { name, ingredients, id, price, emoji } = foodItem
+
+        return `
+        <div class="food-display">
+            <div class="emoji">${emoji}</div>
+                <div class="food-item">
+                    <h2>${name}</h2>
+                    <p>${ingredients}</p>
+                    <h3>$${price}</h3>
+                </div>
+            <i class="fa-solid fa-plus add-item"></i>
+        </div>
+        `
+    })
+}
+
+
+document.getElementById('data-input-el').innerHTML = getFoodItemArr(menuArray)
